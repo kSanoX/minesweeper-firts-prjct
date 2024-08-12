9af7c788c58c020b34935d67df7128b0ba183d5e
Budilenko Olexandr <sahok123444@gmail.com>
2024-08-13 01:25:59 +0300

    commit

--- src/authorization/server.js
+++ src/authorization/server.js
@@ -11,6 +11,7 @@ const corsOptions = {
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 };
 
 app.use(cors(corsOptions));
